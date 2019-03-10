7c44a50bdb0829156c36e361d6addbda881606bc
pavangowda123 <pavan.c38@wipro.com>
2019-03-10 21:43:16 +0530

    updated providers credentials

--- modules/buildmodules/providers.tf
+++ modules/buildmodules/providers.tf
@@ -1,5 +1,5 @@
 provider  "aws"{
-access_key = "AKIAIO2XYZ7VMMMONLWQ"
-secret_key = "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"
+access_key = "AKIAJCHPQH7F3HB3IYDQ"
+secret_key = "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"
 region     = "us-west-1"
 }
